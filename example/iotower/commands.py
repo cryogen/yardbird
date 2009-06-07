eee5b76db0d9dafadcc89a3c32b8e688d60642c1
Nick Moffitt <nick@zork.net>
2009-06-07 01:42:20 +0100

    Made 'key also =verb= value' regex less greedy, to fix appending to factoids

--- example/iotower/commands.py
+++ example/iotower/commands.py
@@ -15,9 +15,8 @@ urlpatterns = patterns('example.iotower.ircviews',
 
  (r"""^(?P<key>.*) \s+ (?P<verb>is|are) \s+ (?P<also>also\s+)? 
   (?P<value>\S+.*) \W*$""", 'learn'),
- (r"""^(?P<key>.*) \s+ (?P<also>also\s+)? =(?P<verb>\w+)= \s+ 
-  (?P<value>\S+.*) \W*$""",
-  'learn'),
+ (r"""^(?P<key>.*?) \s+ (?P<also>also\s+)? =(?P<verb>\w+)= \s+ 
+  (?P<value>\S+.*) \W*$""", 'learn'),
  (r"""^(?P<key>.*)'s \s+ (?P<also>also\s+)? (?P<value>\S+.*) \W*$""",
   'learn'),
 
