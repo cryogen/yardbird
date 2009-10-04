4afeee2fcbc41551b8d4e7c50e28dd3d0a6dfa0c
Nick Moffitt <nick@zork.net>
2009-10-04 22:04:27 +0100

    Untested hail-mary attempt to fix the addressing stuff to actually play nice with the irc addressing detection.

--- yardbird/utils/decorators.py
+++ yardbird/utils/decorators.py
@@ -6,14 +6,12 @@ def require_addressing(function):
     """Decorates a function to return an error if the request neither is
     a private /msg nor contains the bot's nick as the first word."""
     def new(request, *args, **kwargs):
-        if 'addressee' in kwargs:
-            if kwargs['addressee'] == request.my_nick:
-                request.addressed = True
-            else:
-                return render_silence()
         if request.addressed:
             return function(request, *args, **kwargs)
-        return render_error(request,
+        if 'addressee' in kwargs:
+            return render_silence()
+        else:
+            return render_error(request,
                 'You must address me to perform this operation.')
     return new
 
