adf40aed9d1e63191cc438fb092bbeca88987044
Nick Moffitt <nick@zork.net>
2009-11-26 18:01:23 +0000

    more useful test or two

--- iotower/tests.py
+++ iotower/tests.py
@@ -1,8 +1,21 @@
 import unittest
 from yardbird.test import TransactionTestCase
 
-class GoofyTestCase(TransactionTestCase):
-    def test_set_factoid(self):
-        response = self.client.msg(self.client.nickname, 'emad is a troll')
+class FactoidTestCase(TransactionTestCase):
+    def setUp(self):
+        self.client.join('#testing')
+    def _call_and_response(self, assertion, query, substring):
+        response = self.client.msg(self.client.nickname, assertion)
         self.assertContains(response, self.client.my_nickname,
                 method='PRIVMSG')
+        response = self.client.msg(self.client.nickname, query)
+        self.assertContains(response, substring, method='PRIVMSG')
+
+    def test_set_factoid(self):
+        self._call_and_response('emad is a troll', 'what is emad?',
+                'emad is a troll')
+
+    def test_explicit_verb(self):
+        self._call_and_response('apple =hates= your freedom',
+                'what does apple hate?', 'apple hates your freedom')
+
