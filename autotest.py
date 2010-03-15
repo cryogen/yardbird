5fbba239c81f4d4d42f62a763f8261a02c3e4ae0
Space Hobo <spacehobo@spacehobo.com>
2010-03-15 22:29:55 +0000

    * Broke doctest spec out into separate .rst file   * imported doctests into iotower.test as unittest suite * Built out starting infrastructure for reST documentation   * Included pygments-enhanced version of rest2html   * Started drafts of better documentation   * added doc/ trees to manifests * Fixed up premature unlink in autotest.py

--- autotest.py
+++ autotest.py
@@ -30,7 +30,10 @@ def run_tests():
     return True
 
 def generate_coverage():
-    os.unlink('./example/.coverage')
+    try:
+        os.unlink('./example/.coverage')
+    except OSError:
+        pass
     num_failed = call(['python', '../coverage.py', '-x', './manage.py', 'test'], cwd='example')
     reload(coverage)
     cov = coverage.the_coverage
@@ -63,7 +66,6 @@ def coverage_report(cov):
         percentage = 100.0 * total_tested / total_statements
         notify_incomplete_coverage(percentage, report)
     else:
-        print total_tested, total_statements
         notify_complete_coverage()
     return report
 
