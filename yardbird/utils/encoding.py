53a24c387723e2939ade184fea85a5267b2d42e7
Jonas Haggqvist <jonas@rhea>
2010-07-12 00:48:57 +0200

    Test for empty list of encodings, only use the default value if no setting is set AND we weren't passed a list of encodings to use.

--- yardbird/utils/encoding.py
+++ yardbird/utils/encoding.py
@@ -6,11 +6,17 @@ def unicode_fallback(msg, encodings=settings.IRC_INPUT_ENCODINGS):
     """Attempt to decode message into unicode, using the given encodings.
     Defaults to the value of settings.IRC_INPUT_ENCODINGS. or utf-8, then
     CP1252 if settings.IRC_INPUT_ENCODINGS. is not set."""
-    if settings.IRC_INPUT_ENCODINGS == None:
+    # Use a default if the settings is not set, and nothing was passed
+    if settings.IRC_INPUT_ENCODINGS == None and not encodings:
         encodings = ['utf_8', 'cp1252']
+
+    if not encodings: # Some doofus passed [] as value for encodings
+        raise ValueError("encodings must not be an empty list")
+
     for encoding in encodings:
         try:
             return force_unicode(msg, encoding)
         except DjangoUnicodeDecodeError as e:
             last_exception = e
+    # If we haven't returned, raise the last exception (giving up)
     raise last_exception
