0db427d93be7a2e76e460bb8b7d79b671bce6c49
Jonas Häggqvist <rasher@rasher.dk>
2010-07-15 00:41:07 +0200

    Don't throw a hissy-fit if IRC_INPUT_ENCODINGS is not set

--- yardbird/utils/encoding.py
+++ yardbird/utils/encoding.py
@@ -2,16 +2,19 @@
 from django.conf import settings
 from django.utils.encoding import force_unicode, DjangoUnicodeDecodeError
 
-def unicode_fallback(msg, encodings=settings.IRC_INPUT_ENCODINGS):
+def unicode_fallback(msg, encodings=None):
     """Attempt to decode message into unicode, using the given encodings.
     Defaults to the value of settings.IRC_INPUT_ENCODINGS. or utf-8, then
     CP1252 if settings.IRC_INPUT_ENCODINGS. is not set."""
-    # Use a default if the settings is not set, and nothing was passed
-    if settings.IRC_INPUT_ENCODINGS == None and not encodings:
-        encodings = ['utf_8', 'cp1252']
 
-    if not encodings: # Some doofus passed [] as value for encodings
-        raise ValueError("encodings must not be an empty list")
+    # Use a default if the settings is not set
+    if not encodings:
+        encodings = getattr(settings, 'IRC_INPUT_ENCODINGS',
+                ['utf_8', 'cp1252'])
+            
+    if not encodings:
+        raise ValueError("settings.IRC_INPUT_ENCODINGS must not be an empty" +
+                "list")
 
     for encoding in encodings:
         try:
