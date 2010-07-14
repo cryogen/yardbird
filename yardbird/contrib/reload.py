246361071d1b9a1ba7cd878394036b80e0d3f448
Jonas Haggqvist <jonas@rhea>
2010-07-14 14:49:37 +0200

    Move the "Reload succesful" response into a template

--- yardbird/contrib/reload.py
+++ yardbird/contrib/reload.py
@@ -1,9 +1,9 @@
-from yardbird.irc import IRCResponse
+from yardbird.shortcuts import render_to_response
 from yardbird.utils.decorators import require_addressing, require_chanop
 
 @require_addressing
 @require_chanop
 def reload(request, *args, **kwargs):
-    return IRCResponse(request.reply_recipient, 'Reload successful.',
+    return render_to_response(request.reply_recipient, "reload.irc", {},
             method='RESET')
 
