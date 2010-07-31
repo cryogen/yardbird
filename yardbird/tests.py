4f8822bb4b897660899836b8bad20280d957a9fb
Nick Moffitt <nick@zork.net>
2010-08-01 00:26:31 +0100

    * Reworked the bot unit tests to use mox instead of pymock, to much rejoicing * Stubbed the living daylights out of twisted to get serial execution of the asynchronous work for these tests

--- yardbird/tests.py
+++ yardbird/tests.py
@@ -1,4 +1,5 @@
-import unittest, mock
+import mox
+from twisted import trial
 
 from yardbird.test import TestCase
 
@@ -32,88 +33,92 @@ class TestCaseTestCase(TestCase):
                 count=1)
         self.assertNotContains(response, "sandwiches", method='PRIVMSG')
 
-class LineMocker(mock.Mock):
-    def assert_line_sent(self, line):
-        assert ((line,),{}) in self.call_args_list, \
-                'Expected: %s\nCalled with: %s' % (line,
-                    self.call_args_list)
-    def assert_next_line(self, line):
-        assert self.call_args_list[0][0][0] == line, \
-                'Expected: %s\nCalled with: %s' % (line,
-                    self.call_args_list[0][0][0])
-        return self.call_args_list.pop(0)
-    def assert_quiet(self):
-        assert self.call_args_list == [], \
-                '%s called unexpectedly with %s' % (self._name,
-                        self.call_args_list[0][0][0])
-
-class DjangoBotIrcTestCase(unittest.TestCase):
+class DjangoBotIrcTestCase(mox.MoxTestBase):
     """Use mocks to test the IRC activity of the bot object."""
     def setUp(self):
-        yardbird.bot.log = mock.Mock()
+        """This test case's setup includes a heavily stubbed-out
+        DjangoBot object.  We manually stub out the two twisted methods
+        that were used to defer/multi-thread execution, causing flow to
+        execute sequentially.  The logging and protocol send functions
+        are mocked for our test verification, and we make a bogus
+        factory object to communicate the test bot object's settings."""
+        super(DjangoBotIrcTestCase, self).setUp()
+        import logging
+        from twisted.internet import ssl
+        yardbird.bot.defer.inlineCallbacks = lambda f,a,k: list(f(*a,**k))
+        yardbird.bot.threads.deferToThread = lambda f,r,a,k: f(r,a,k)
+        yardbird.bot.log = self.mox.CreateMock(logging._loggerClass)
         self.bot = yardbird.bot.DjangoBot()
-        self.bot.sendLine = LineMocker(name='sendLine')
-        self.bot.factory = mock.Mock()
-        self.bot.factory.nickname = 'testbot'
+        self.mox.StubOutWithMock(self.bot, "sendLine")
+        self.bot.factory = self.mox.CreateMock(
+                ssl.DefaultOpenSSLContextFactory)
+        self.bot.factory.protocol = yardbird.bot.DjangoBot
         self.bot.factory.password = 'secret'
+        self.bot.factory.nickname = 'testbot'
         self.bot.factory.channels = ('#yardbird', '#testing')
         self.bot.factory.privchans = ('#yardbird',)
-        self.bot.sendLine.assert_quiet()
+        self.bot.factory.hostname = 'irc.example.com'
+        self.bot.factory.port = 6667
     def test_myInfo(self):
-        self.bot.sendLine.assert_quiet()
-        self.bot.myInfo('testsvr', 'mockirc', 'foo', 'bar')
-        self.bot.sendLine.assert_next_line('PING testsvr')
-        self.bot.sendLine.assert_quiet()
+        servername = 'testsvr'
+        yardbird.bot.log.info("Connected to %s" % servername)
+        yardbird.bot.log.debug('PING %s' % servername)
+        self.bot.sendLine('PING %s' % servername)
+        self.mox.ReplayAll()
+
+        self.bot.myInfo(servername, 'mockirc', 'hostname', 'servername')
     def test_connectionMade(self):
-        self.bot.sendLine.assert_quiet()
+        self.bot.sendLine('PASS %s' % self.bot.factory.password)
+        self.bot.sendLine('NICK %s' % self.bot.factory.nickname)
+        self.bot.sendLine('USER %s foo bar :%s' %
+                (self.bot.factory.nickname, self.bot.realname))
+        self.mox.ReplayAll()
+
         self.bot.connectionMade()
-        self.bot.sendLine.assert_next_line('PASS %s' %
-                self.bot.factory.password)
-        self.bot.sendLine.assert_next_line('NICK %s' %
-                self.bot.factory.nickname)
-        self.bot.sendLine.assert_next_line(
-                'USER %s foo bar :%s' % (self.bot.nickname,
-                    self.bot.realname))
-        self.bot.sendLine.assert_quiet()
     def test_signedOn(self):
-        self.bot.sendLine.assert_quiet()
-        self.bot.signedOn()
-        self.bot.sendLine.assert_next_line(
-                'PRIVMSG %s :Watching for my own hostmask' %
+        self.bot.sendLine('PRIVMSG %s :Watching for my own hostmask' %
                 self.bot.nickname)
-        self.bot.sendLine.assert_next_line('JOIN #yardbird')
-        self.bot.sendLine.assert_next_line('JOIN #testing')
-        self.bot.sendLine.assert_quiet()
+        for chan in self.bot.factory.channels:
+            self.bot.sendLine('JOIN %s' % chan)
+        self.mox.ReplayAll()
+
+        self.bot.signedOn()
     def test_ping(self):
-        self.bot.sendLine.assert_quiet()
+        self.bot.sendLine('PONG AA4B4873')
+        self.mox.ReplayAll()
+
         self.bot.lineReceived('PING :AA4B4873')
-        self.bot.sendLine.assert_next_line('PONG AA4B4873')
-        self.bot.sendLine.assert_quiet()
     def test_joined(self):
-        self.bot.sendLine.assert_quiet()
-        self.bot.joined('#yardbird')
-        self.bot.sendLine.assert_next_line('WHO #yardbird')
-        self.bot.joined('#testing')
-        self.bot.sendLine.assert_next_line('WHO #testing')
-        self.bot.sendLine.assert_quiet()
+        for chan in self.bot.factory.channels:
+            yardbird.bot.log.info("[I have joined %s]" % chan)
+            self.bot.sendLine('WHO %s' % chan)
+        self.mox.ReplayAll()
+
+        for chan in self.bot.factory.channels:
+            self.bot.joined(chan)
     def test_noticed(self):
-        self.bot.sendLine.assert_quiet()
         self.bot.noticed('whatever')
-        self.bot.sendLine.assert_quiet()
+        self.mox.ReplayAll()
     def test_connectionLost(self):
-        self.bot.sendLine.assert_quiet()
-        self.bot.connectionLost('The end. No moral!')
+        msg = 'The end. No moral!'
+        yardbird.bot.log.warn('Disconnected from %s (%s:%d): %s' %
+                (self.bot.servername, self.bot.factory.hostname,
+                    self.bot.factory.port, msg))
+        self.mox.ReplayAll()
+
+        self.bot.connectionLost(msg)
         self.assertEqual(self.bot.connected, 0,
                 'Bot did not notice loss of connection')
-        self.bot.sendLine.assert_quiet()
-    #def test_defer(self):
-        #self.bot.sendLine.assert_quiet()
-        #yardbird.bot.threads.deferToThread = mock.Mock(
-                #'twisted.internet.threads.deferToThread')
-        #yardbird.bot.defer = mock.Mock('twisted.internet.defer')
-        #self.bot.privmsg('SpaceHobo', '#yardbird', 'What is real?')
-        #print self.bot.sendLine.call_args_list
-        #print yardbird.bot.threads.deferToThread.method_calls, yardbird.bot.threads.deferToThread.call_args_list
-        #print yardbird.bot.defer.method_calls, yardbird.bot.defer.call_args_list
+    def test_defer(self):
+        nick = 'SpaceHobo'
+        chan = '#yardbird'
+        msg = 'stats'
+        reply = 'You must address me to perform this operation.'
 
+        yardbird.bot.log.info(u'%s: <%s> %s' % (chan, nick, msg))
+        yardbird.bot.log.info(u'NOTICE: <%s> %s' % (nick, reply))
+        self.bot.sendLine('NOTICE %s :%s' % (nick, reply))
+        self.mox.ReplayAll()
+
+        self.bot.privmsg(nick, chan, msg)
 
