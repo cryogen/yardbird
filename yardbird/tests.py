88935c53bb890b147ac7a771834f264bd11590dc
Nick Moffitt <nick@zork.net>
2010-08-01 17:17:17 +0100

    * Gave in on ever stubbing out twisted successfully.  Fortunately this is limited to the dispatch method, which I removed from coverage calculations.  Unfortunately I had to work around the coverage tool a bit. * re-worked mox tests quite a bit to get us up to 78% coverage of bot.py.  The rest is just simple work, I hope.

--- yardbird/tests.py
+++ yardbird/tests.py
@@ -1,9 +1,12 @@
 import mox
-from twisted import trial
+
+from django.http import Http404
+from django.core import exceptions
 
 from yardbird.test import TestCase
 
 import yardbird.bot
+import yardbird.irc
 from yardbird.contrib import shortener
 
 
@@ -44,12 +47,15 @@ class DjangoBotIrcTestCase(mox.MoxTestBase):
         factory object to communicate the test bot object's settings."""
         super(DjangoBotIrcTestCase, self).setUp()
         import logging
-        from twisted.internet import ssl
-        yardbird.bot.defer.inlineCallbacks = lambda f,a,k: list(f(*a,**k))
-        yardbird.bot.threads.deferToThread = lambda f,r,a,k: f(r,a,k)
+        from twisted.internet import ssl, defer
+        from twisted.python import failure
         yardbird.bot.log = self.mox.CreateMock(logging._loggerClass)
+        self.bogus_deferred = self.mox.CreateMock(defer.Deferred)
+        self.bogus_failure = self.mox.CreateMock(failure.Failure)
+
         self.bot = yardbird.bot.DjangoBot()
         self.mox.StubOutWithMock(self.bot, "sendLine")
+        self.mox.StubOutWithMock(self.bot, "dispatch")
         self.bot.factory = self.mox.CreateMock(
                 ssl.DefaultOpenSSLContextFactory)
         self.bot.factory.protocol = yardbird.bot.DjangoBot
@@ -76,13 +82,16 @@ class DjangoBotIrcTestCase(mox.MoxTestBase):
 
         self.bot.connectionMade()
     def test_signedOn(self):
-        self.bot.sendLine('PRIVMSG %s :Watching for my own hostmask' %
-                self.bot.nickname)
+        mask = '%s!%s@example.com' % (self.bot.nickname, self.bot.nickname)
+        msg = 'Watching for my own hostmask'
+
+        self.bot.sendLine('PRIVMSG %s :%s' % (self.bot.nickname, msg))
         for chan in self.bot.factory.channels:
             self.bot.sendLine('JOIN %s' % chan)
         self.mox.ReplayAll()
 
         self.bot.signedOn()
+        self.bot.privmsg(mask, mask, msg)
     def test_ping(self):
         self.bot.sendLine('PONG AA4B4873')
         self.mox.ReplayAll()
@@ -112,13 +121,76 @@ class DjangoBotIrcTestCase(mox.MoxTestBase):
     def test_defer(self):
         nick = 'SpaceHobo'
         chan = '#yardbird'
-        msg = 'stats'
-        reply = 'You must address me to perform this operation.'
+        msg = 'What is real?'
 
         yardbird.bot.log.info(u'%s: <%s> %s' % (chan, nick, msg))
-        yardbird.bot.log.info(u'NOTICE: <%s> %s' % (nick, reply))
-        self.bot.sendLine('NOTICE %s :%s' % (nick, reply))
+        self.bot.dispatch(mox.IsA(yardbird.irc.IRCRequest)
+                ).AndReturn(self.bogus_deferred)
+        self.bogus_deferred.addErrback(yardbird.bot.report_error,
+                self.bot, mox.IsA(yardbird.irc.IRCRequest)
+                ).AndReturn(self.bogus_deferred)
+        self.bogus_deferred.addErrback(yardbird.bot.unrecoverable_error,
+                self.bot, mox.IsA(yardbird.irc.IRCRequest)
+                ).AndReturn(self.bogus_deferred)
         self.mox.ReplayAll()
 
-        self.bot.privmsg(nick, chan, msg)
+        d = self.bot.action(nick, chan, msg)
+    def test_404(self):
+        nick = 'SpaceHobo'
+        chan = '#yardbird'
+        msg = '%s: What is real?' % self.bot.nickname
+
+        self.bogus_failure.trap(Http404, exceptions.PermissionDenied,
+                exceptions.ValidationError).AndReturn(Http404)
+        yardbird.bot.log.debug(self.bogus_failure)
+        self.bot.sendLine("PRIVMSG %s :I don't know what you mean, %s."
+                % (chan, nick))
+        self.mox.ReplayAll()
+
+        yardbird.bot.report_error(self.bogus_failure, self.bot,
+                yardbird.irc.IRCRequest(self.bot, nick, chan, msg,
+                'privmsg'))
+    def test_unaddressed_404(self):
+        nick = 'SpaceHobo'
+        chan = '#yardbird'
+        msg = 'What is real?'
+
+        self.bogus_failure.trap(Http404, exceptions.PermissionDenied,
+                exceptions.ValidationError).AndReturn(Http404)
+        yardbird.bot.log.debug(self.bogus_failure)
+        self.mox.ReplayAll()
+
+        yardbird.bot.report_error(self.bogus_failure, self.bot,
+                yardbird.irc.IRCRequest(self.bot, nick, chan, msg,
+                'privmsg'))
+    def test_403(self):
+        nick = 'SpaceHobo'
+        chan = '#yardbird'
+        msg = '%s: delete the Internet!' % self.bot.nickname
+
+        self.bogus_failure.trap(Http404, exceptions.PermissionDenied,
+                exceptions.ValidationError
+                ).AndReturn(exceptions.PermissionDenied)
+        yardbird.bot.log.debug(self.bogus_failure)
+        self.bot.sendLine("PRIVMSG %s :Sorry, %s." % (chan, nick))
+        self.mox.ReplayAll()
+
+        yardbird.bot.report_error(self.bogus_failure, self.bot,
+                yardbird.irc.IRCRequest(self.bot, nick, chan, msg,
+                'privmsg'))
+    def test_unrecoverable_error(self):
+        nick = 'SpaceHobo'
+        chan = '#yardbird'
+        msg = '%s: delete the Internet!' % self.bot.nickname
+
+        yardbird.bot.log.warn(self.bogus_failure)
+        self.bogus_failure.getErrorMessage().AndReturn('INTERNET DELETED!')
+        self.bot.sendLine("NOTICE %s :INTERNET DELETED!" % chan)
+        self.mox.ReplayAll()
+
+        yardbird.bot.unrecoverable_error(self.bogus_failure, self.bot,
+                yardbird.irc.IRCRequest(self.bot, nick, chan, msg,
+                'privmsg'))
+
+
 
